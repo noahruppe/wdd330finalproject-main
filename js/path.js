24e5f50d2e98ac4ea73e693b6ab7ceffa90719e5
noahruppe <noah.ruppe30@gmail.com>
2024-06-01 17:50:57 -0400

    s

--- js/path.js
+++ js/path.js
@@ -1,6 +1,8 @@
+// Check if the current page is index.html
 if (window.location.pathname.endsWith('index.html')) {
-    // Get the favorites link element
-    var favoritesLink = document.getElementById('favorites');
-    // Update the href attribute to point to index.html
-    favoritesLink.href = 'favorites/favorites.html';
+    // Update the href attribute of the favorites link to point to favorites.html in the parent directory
+    document.getElementById('favorites').href = '../favorites/favorites.html';
+} else {
+    // Update the href attribute of the favorites link to point to favorites.html in the current directory
+    document.getElementById('favorites').href = 'favorites.html';
 }
