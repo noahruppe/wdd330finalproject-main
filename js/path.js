c36c3c046e0c14b9c33dd4b106ce398665cab482
noahruppe <noah.ruppe30@gmail.com>
2024-06-01 18:18:59 -0400

    s

--- js/path.js
+++
@@ -1,8 +0,0 @@
-// Check if the current page is index.html
-if (window.location.pathname.endsWith('index.html')) {
-    // Update the href attribute of the favorites link to point to favorites.html in the parent directory
-    document.getElementById('favorites').href = '../favorites/favorites.html';
-} else {
-    // Update the href attribute of the favorites link to point to favorites.html in the current directory
-    document.getElementById('favorites').href = 'favorites.html';
-}
